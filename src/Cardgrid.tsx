d57b50ee95bbd71ff7a1729f02738ce102bd6b00
Petri <meikyo72@gmail.com>
2022-03-26 15:11:37 +0100

    Implemented modal view for cards

--- src/Cardgrid.tsx
+++ src/Cardgrid.tsx
@@ -1,9 +1,10 @@
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import { Typography } from '@mui/material';
 
 const modalstyle = {
     position: 'absolute' as 'absolute',
@@ -17,13 +18,13 @@ const modalstyle = {
   };
 
 
-export default function Cardgrid(){
+  function Cardgrid(){
     const [card, setCard] = useState([])
     const [ima, setIma] = useState([])
     const [open, setOpen] = useState(false);
+    const [modalCard, setmodalCard] = useState<string>()
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const cardElement = useRef<HTMLImageElement>(null);
 
   const getCards = async () => {
     const response = await fetch('https://api.magicthegathering.io/v1/cards')
@@ -34,29 +35,39 @@ export default function Cardgrid(){
     setIma(image)
   }
 
+ 
 
   useEffect(()=>{
     getCards();
   },[])
 
-  function cardClick(){
-    handleOpen();
-  }
+  useEffect(()=>{
+    if (modalCard)
+    handleOpen()
+  },[modalCard])
  
+  
+  function cardClick(event: React.MouseEvent<HTMLImageElement>) {
+    event.stopPropagation();
+    const img = event.currentTarget;
+    const modalImage:string = img.src
+    setmodalCard(modalImage)
+    }
   
   return(
     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>     
-        {card.map((name,index)=><Grid item xs={2} sm={4} md={4} key={index} >{name} <br/> <img  src={ima[index]} alt="CardImage" ref={cardElement}/><br/><Button onClick={cardClick} key={index} variant="contained">Add to Cart</Button>
-        <Modal
-        open={open}
-        onClose={handleClose}
-        aria-describedby="modal-modal-image"
-      >
-        <Box sx={modalstyle}>
-            <div>{index}</div>
-        </Box>
-      </Modal>
+        {card.map((name,index)=><Grid item xs={2} sm={4} md={4} key={index} ><Typography variant='h6'>{name}</Typography> <br/> <img  src={ima[index]} onClick={cardClick} alt="CardImage" /><br/><Button key={index} variant="contained">Add to Cart</Button>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-describedby="modal-modal-image"
+    >
+      <Box sx={modalstyle}>
+          <img src={modalCard} alt="ModalImage" />
+      </Box>
+    </Modal>  
       </Grid>)}
       </Grid>
   );
 }
+export default Cardgrid
