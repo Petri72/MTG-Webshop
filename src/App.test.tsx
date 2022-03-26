d0617c9bcc670dc6eb33935a0709cfe6620322c0
Petri <meikyo72@gmail.com>
2022-03-26 15:24:23 +0100

    Added test for Cardgrid

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,7 @@ import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import {expect} from 'chai';
 import Grid from '@mui/material/Grid';
+import Cardgrid from './Cardgrid';
 
 configure({ adapter: new Adapter() });
 
@@ -13,9 +14,9 @@ it('renders without a crash', () => {
   shallow(<App />)
 });
 
-describe('<App />', () => {
+describe('<Cardgrid />', () => {
   it('renders Grid', () => {
-    const component=shallow(<App />)
+    const component=shallow(<Cardgrid />)
      expect (component.containsMatchingElement(<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}></Grid>)).to.equal(true)
   })
 })
